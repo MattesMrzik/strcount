1f27523fb3a4bba07752ec8e23ad42fb1a1fba5b
MattesMrzik <mattes@mrzik.de>
2024-05-02 13:56:17 -0400

    test both coordinate systems

---
+++ src/STRcount/test/resources/README.md
@@ -0,0 +1,5 @@
+## Contains files used for testing of:
+- get_genome_str_graph()
+  - `ref.fa` $\rightarrow$ the reference genome
+  - `config_db.tsv` $\rightarrow$ the config file that uses 0-based indexing and exclusive end
+  - `config_browser.tsv` $\rightarrow$ the config file that uses 1-based indexing and inclusive end
--- src/STRcount/test/tests.py
+++ src/STRcount/test/tests.py
@@ -5,9 +5,9 @@ sys.path.append('..')
 from genome_str_graph_generator import get_genome_str_graph
 
 class Test_get_genome_str_graph(unittest.TestCase):
-    def test_get_genome_str_graph(self):
+    def test_get_genome_str_graph_browser_coods(self):
         segments, links = get_genome_str_graph(
-            "resources/config.tsv", "resources/ref.fa", "+", "+", "+", False, True, False)
+            "resources/config_browser.tsv", "resources/ref.fa", "+", "+", "+", False, True, False)
         true_segments = [["S", "ref_before_prefix_1", "ACGTAACCGGT"],
                          ["S", "ref_prefix_1", "TAAACCCGGGTTT"],
                          ["S", "repeat_1", "AA"],
@@ -21,6 +21,23 @@ class Test_get_genome_str_graph(unittest.TestCase):
                       ["L", "repeat_1", "+", "ref_suffix_1", "+", "0M"],
                       ["L", "ref_suffix_1", "+", "ref_after_suffix_1", "+", "0M"]]
         self.assertEqual(links["ref_1"], true_links)
+
+    def test_get_genome_str_graph_db_coods(self):
+      segments, links = get_genome_str_graph(
+          "resources/config_db.tsv", "resources/ref.fa", "+", "+", "+", False, False, False)
+      true_segments = [["S", "ref_before_prefix_1", "ACGTAACCGGT"],
+                        ["S", "ref_prefix_1", "TAAACCCGGGTTT"],
+                        ["S", "repeat_1", "AA"],
+                        ["S", "ref_suffix_1", "CCCCGGGGT"],
+                        ["S", "ref_after_suffix_1", "TTT"]]
+      self.assertEqual(segments["ref_1"], true_segments)
+
+      true_links = [["L", "ref_before_prefix_1", "+", "ref_prefix_1", "+", "0M"],
+                    ["L", "ref_prefix_1", "+", "repeat_1", "+", "0M"],
+                    ["L", "repeat_1", "+", "repeat_1", "+", "0M"],
+                    ["L", "repeat_1", "+", "ref_suffix_1", "+", "0M"],
+                    ["L", "ref_suffix_1", "+", "ref_after_suffix_1", "+", "0M"]]
+      self.assertEqual(links["ref_1"], true_links)
 
 
 if __name__ == '__main__':
